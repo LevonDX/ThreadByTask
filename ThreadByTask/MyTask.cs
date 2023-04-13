ba4a811794b6b5f3ae1ae24a1f8c767986319cb1
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-04-13 20:34:33 +0400

    Add project files.

---
+++ ThreadByTask/MyTask.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadByTask
+{
+    class MyTask<T>
+    {
+        Func<T> work;
+        public Thread thread;
+
+        public MyTask(Func<T> work)
+        {
+            this.work = work;
+            thread = new Thread(() => { work(); });
+            thread.IsBackground = true;
+        }
+
+        public void Start()
+        {
+            thread.Start();
+        }
+
+        public MyTaskAwaiter<T> GetAwaiter()
+        {
+            return new MyTaskAwaiter<T>(this);
+        }
+
+    }
+
+    class MyTaskAwaiter<T>
+    {
+        MyTask<T> task;
+
+        public MyTaskAwaiter(MyTask<T> task)
+        {
+            this.task = task;
+        }
+
+        public void OnCompleted(Action action)
+        {
+            task.thread.Join();
+
+            action();
+        }
+    }
+}
---
+++ ThreadByTask/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ThreadByTask
+{
+    internal class Program
+    {
+        static long F()
+        {
+            Console.WriteLine("F started");
+
+            long sum = 0;
+            for (long i = 0; i <= 5E9; i++)
+            {
+                sum += i;
+            }
+
+            return sum;
+        }
+
+        static void TaskCompleted()
+        {
+            Console.WriteLine("Task completed");
+        }
+
+        static void Main(string[] args)
+        {
+            MyTask<long> myTask = new MyTask<long>(F);
+            myTask.Start();
+
+            MyTaskAwaiter<long> taskAwaiter = myTask.GetAwaiter();
+
+            taskAwaiter.OnCompleted(TaskCompleted);
+
+            Console.WriteLine("Main finished");
+            Console.ReadLine();
+        }
+    }
+}
